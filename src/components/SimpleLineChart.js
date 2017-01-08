f51479df12ced6793547bd3681827fdf083dfd7e
lzys90 <leongyzs@gmail.com>
2017-01-08 19:13:49 +0800

    Add storing of historical data state

--- src/components/SimpleLineChart.js
+++ src/components/SimpleLineChart.js
@@ -13,22 +13,21 @@ export default class SimpleLineChart extends Component {
 
     componentWillReceiveProps (nextProps) {
         let tmpData = [{name: this.props.timeID, usage: this.props.usage}];
-        // TODO: store prev data
         this.setState({
-            data: tmpData
+            data: this.state.data.concat(tmpData)
         })
     }
 
     render () {
         return (
-            <LineChart width={300} height={200} data={this.state.data}
+                <LineChart width={500} height={200} data={this.state.data}
                 margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-               <XAxis dataKey="name"/>
+               <XAxis dataKey="name" label="Time" />
                <YAxis/>
                <CartesianGrid strokeDasharray="3 3"/>
                <Tooltip/>
                <Legend />
-               <Line type="monotone" dataKey="usage" stroke="#8884d8" activeDot={{r: 8}}/>
+               <Line name="No. of Cars in Use" type="monotone" dataKey="usage" stroke="#8884d8" activeDot={{r: 8}}/>
             </LineChart>
         )
     }
